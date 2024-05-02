5a9f32c9c3ff19c9ca8835423ecf2b93dcc2b5f2
HardikROR <hardik.scs96@gmail.com>
2024-05-02 18:16:18 +0530

    Added method for string calcultion with validation

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 require_relative '../string_calculator'
+require 'pry'
 
 describe StringCalculator do
+  describe '#add' do
+    context 'method input' do
+      it 'expects string in argument' do
+        expect { described_class.add('1') }.to_not raise_error
+        expect { described_class.add(1) }.to raise_error(RuntimeError, 'Argument must be a string')
+      end
+
+      it 'expects only one argument' do
+        expect { described_class.add(1, 2) }.to raise_error(ArgumentError)
+      end
+    end
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class StringCalculator
+  def self.add(string_of_numbers)
+    raise 'Argument must be a string' unless string_of_numbers.is_a?(String)
+  end
 end
